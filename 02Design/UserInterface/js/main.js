6215f954eb66234296ef4f937b41b170a551e1be
purnama <arthur@purnama.de>
2013-02-08 00:15:51 +0100

    initial

--- 02Design/UserInterface/js/main.js
+++ 02Design/UserInterface/js/main.js
@@ -2,14 +2,14 @@ function resetHomeContainer() {
 
     var height = $(window).height();
 
-   if(height > 150){
-       var newHeight = (height-250)/2;
-       if(newHeight > 150){
-        $('#home-container').attr("style", "margin-top: " + newHeight + "px;");
-       }else{
-           $('#home-container').removeAttr("style");
-       }
-   }else{
-       $('#home-container').removeAttr("style");
-   }
+    if (height > 150) {
+        var newHeight = (height - 250) / 2;
+        if (newHeight > 150) {
+            $('#home-container').attr("style", "margin-top: " + newHeight + "px;");
+        } else {
+            $('#home-container').removeAttr("style");
+        }
+    } else {
+        $('#home-container').removeAttr("style");
+    }
 }
