aa9833b872b23e625c73bfe085dc31dfdacfd26a
Arthur Purnama <arthur@purnama.de>
2013-03-25 00:51:25 +0100

    Create Default Bundle

--- src/Kateglo/DefaultBundle/Controller/DefaultController.php
+++ src/Kateglo/DefaultBundle/Controller/DefaultController.php
@@ -2,18 +2,51 @@
 
 namespace Kateglo\DefaultBundle\Controller;
 
+use Kateglo\DefaultBundle\Service\BaseLink;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use JMS\DiExtraBundle\Annotation\InjectParams;
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\View;
+use Symfony\Component\BrowserKit\Response;
 
 class DefaultController extends Controller
 {
+    /**
+     * @var BaseLink
+     */
+    private $baseLink;
+
+    /**
+     * @param BaseLink $baseLink
+     * @InjectParams
+     */
+    public function __construct(BaseLink $baseLink){
+        $this->baseLink = $baseLink;
+    }
+    
     /**
      * @Get("/")
      * @View()
      */
     public function indexAction()
     {
-        return array('name' => 'test');
+        $response = $this->baseLink->get($this);
+        $response['form'] = array(
+            'search' => array(
+                'name' => 'search',
+                'action' => 'entri.html',
+                'method' => 'get',
+                'field' => array(
+                    array(
+                        'name' => 'query',
+                        'type' => 'search'
+                    ),
+                ),
+            )
+        );
+
+        return $response;
     }
+
 }
